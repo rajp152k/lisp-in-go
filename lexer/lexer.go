5539a0bb5b36b0e3886a88c86fd65b0dc06fe56b
(Bit-Mage) <raj@thebitmage.com>
2024-10-06 21:18:17 +0530

    green(lexer): strings lexable now
    Signed-off-by: (Bit-Mage) <raj@thebitmage.com>

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,6 @@
 package lexer
 
+
 func Lexer(source string) []Token {
 	lexed := []Token{}
 	var acc string
@@ -12,8 +13,24 @@ func Lexer(source string) []Token {
 		}
 	}
 
+
 	for i := 0; i < len(source); i++ {
 		curr = string(source[i])
+		if curr == "|"{
+			resetAcc()
+			for j := i+1; j < len(source); j++{
+				currj := string(source[j])
+				if currj != "|"{
+					acc += currj
+				} else {
+					lexed = append(lexed, *newString(acc))
+					acc = ""
+					i = j
+					break
+				}
+			}
+			continue
+		}
 		if isSpecial(curr) {
 			resetAcc()
 			lexed = append(lexed, *getSpecial[curr])
